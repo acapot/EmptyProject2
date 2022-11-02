6a9ba8d2c0c2c1b6fe91e12495205d0c290397ba
Alexis Capot <alexiscapot@hotmail.com>
2022-11-02 18:54:35 +0100

    Add project files.

---
+++ EmptyProject2/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using EmptyProject2.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace EmptyProject2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+
+        public IActionResult Projects()
+        {
+            return View();
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ EmptyProject2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EmptyProject2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
